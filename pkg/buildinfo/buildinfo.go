745832ae8b836e487f55f95137f4e088fff2a598
Tamás Benke <tombenke@gmail.com>
2025-10-02 15:13:01 +0200

    Finish initial version of buildinfo package

--- pkg/buildinfo/buildinfo.go
+++ pkg/buildinfo/buildinfo.go
@@ -6,8 +6,13 @@ import (
 	"strings"
 )
 
+// Provides the complete debug.BuildInfo struct
 var BuildInfo *debug.BuildInfo
+
+// A dictionary with keys that are listed with the `-tags ...` switch during the build process
 var BuildTags map[string]struct{} = make(map[string]struct{})
+
+// It will be true of the build process is executed with the `-tags DEBUG` flag, otherwise it is false
 var DebugMode bool
 
 func init() {
@@ -19,7 +24,6 @@ func init() {
 	fmt.Printf("BuildInfo: %+v\n", BuildInfo.Settings)
 	for _, setting := range BuildInfo.Settings {
 		if setting.Key == "-tags" {
-			// A tag-ek szóközökkel elválasztva szerepelnek
 			tags := strings.Fields(setting.Value)
 			for _, tag := range tags {
 				BuildTags[tag] = struct{}{}
